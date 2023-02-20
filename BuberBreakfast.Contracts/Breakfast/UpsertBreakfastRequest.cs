c446bd4b758de5b0ea3ccbbce6114eb982943bd9
Pawel Mroz <pawel.mroz@adminbrothers.com>
2023-02-20 12:22:59 +0100

    Begining of project

---
+++ BuberBreakfast.Contracts/Breakfast/UpsertBreakfastRequest.cs
@@ -0,0 +1,9 @@
+namespace BuberBreakfast.Contracts;
+
+public record UpsertBreakfastRequest(
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    List<string> Savory,
+    List<string> Sweet);
